c4a262feb978f8e59682404adf3e6dba7215b690
Matti Lupari <matti.lupari@arcusys.fi>
2017-12-09 01:53:20 +0200

    Make 8b a bit more functional:

--- src/main/scala/challenge/Day8b.scala
+++ src/main/scala/challenge/Day8b.scala
@@ -19,20 +19,22 @@ object Day8b extends Challenge {
 
     private val _amount = args.dropWhile(c => !c.isDigit && !c.equals('-')).takeWhile(c => c.isDigit || c.equals('-'))
     private val _direction = if (args.contains("inc")) 1 else -1
-    private val _condPart = args.split(" if ").last.split(" ")
 
+    private val cond = args.split(" if ").last.split(" ")
     private val dest = args.takeWhile(c => !c.isWhitespace)
     private val n = _amount.toInt * _direction
-    private val condition: (String, String, Int) = (_condPart(0), _condPart(1), _condPart(2).toInt)
 
-    def exec(): Unit = condition match {
-      case (r, "<", v) => if (registry(r)._1 < v) incRegistry(dest, n)
-      case (r, ">", v) => if (registry(r)._1 > v) incRegistry(dest, n)
-      case (r, "<=", v) => if (registry(r)._1 <= v) incRegistry(dest, n)
-      case (r, ">=", v) => if (registry(r)._1 >= v) incRegistry(dest, n)
-      case (r, "==", v) => if (registry(r)._1 == v) incRegistry(dest, n)
-      case (r, "!=", v) => if (registry(r)._1 != v) incRegistry(dest, n)
-      case _ => println("unknown cmd " + condition)
+    private val condFn = cond(1) match {
+      case "<" => (r: Int, v: Int) => {r < v}
+      case ">" => (r: Int, v: Int) => {r > v}
+      case "<=" => (r: Int, v: Int) => {r <= v}
+      case ">=" => (r: Int, v: Int) => {r >= v}
+      case "==" => (r: Int, v: Int) => {r == v}
+      case "!=" => (r: Int, v: Int) => {r != v}
+    }
+
+    def exec(): Unit = {
+      if (condFn(registry(cond(0))._1, cond(2).toInt)) incRegistry(dest, n)
     }
   }
 
