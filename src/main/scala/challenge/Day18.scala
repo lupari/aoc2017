89991e20efe86f4892455885740588c4743141e9
Matti Lupari <matti.lupari@arcusys.fi>
2017-12-18 22:22:07 +0200

    cleanup

--- src/main/scala/challenge/Day18.scala
+++ src/main/scala/challenge/Day18.scala
@@ -2,112 +2,97 @@ package challenge
 
 import base.Challenge
 
+import scala.annotation.tailrec
+import scala.collection.mutable
 import scala.io.Source
 
 object Day18 extends Challenge {
 
-  case class State(position: Long, registry: Map[Char, Long], isBlocked: Boolean) {
-    def inc(): State = copy(position = position + 1)
+  val registry: mutable.Map[Char, Long] = mutable.Map[Char, Long]('_' -> 0).withDefaultValue(0)
 
-    def setRegister(r: Char, value: Long): State = copy(registry = registry + (r -> value))
+  sealed trait Instruction {
+    def exec(): Long
 
-    def getValue(arg: String): Long = if (arg.last.isDigit) arg.toInt else registry.getOrElse(arg.last, 0)
+    def isRcv: Boolean = false
 
-    def getValue(arg: Char): Long = registry.getOrElse(arg, 0)
+    def resolve(arg: String): Long = if (arg.last.isDigit) arg.toInt else registry(arg.last)
   }
 
-  object State {
-    def empty(): State = State(0, Map(), isBlocked = false)
-  }
-
-  trait Instruction {
-    def exec(state: State): State
-  }
-
-  case class Jmp(x: Char, y: String) extends Instruction {
-    override def exec(state: State): State = {
-      val inc = if (state.getValue(x) > 0) state.getValue(y) else 1
-      state.copy(position = state.position + inc)
-    }
+  case class Jmp(x: String, y: String) extends Instruction {
+    override def exec(): Long = if (resolve(x) > 0) resolve(y) else 1
   }
 
   case class Snd(x: Char) extends Instruction {
-    override def exec(state: State): State = {
-      state.inc().setRegister('_', state.getValue(x))
-    }
-  }
-
-  case class Rcv(x: Char) extends Instruction {
-    override def exec(state: State): State = {
-      val inc = if (state.getValue(x) > 0) 1 else 0
-      if (inc == 1) state.copy(isBlocked = true) else state.inc()
+    override def exec(): Long = {
+      registry('_') = registry(x)
+      1
     }
   }
 
   case class Set(x: Char, y: String) extends Instruction {
-    override def exec(state: State): State = {
-      state.inc().setRegister(x, state.getValue(y))
+    override def exec(): Long = {
+      registry(x) = resolve(y)
+      1
     }
   }
 
   case class Add(x: Char, y: String) extends Instruction {
-    override def exec(state: State): State = {
-      state.inc().setRegister(x, state.getValue(x) + state.getValue(y))
+    override def exec(): Long = {
+      registry(x) += resolve(y)
+      1
     }
   }
 
   case class Mul(x: Char, y: String) extends Instruction {
-    override def exec(state: State): State = {
-      state.inc().setRegister(x, state.getValue(x) * state.getValue(y))
+    override def exec(): Long = {
+      registry(x) *= resolve(y)
+      1
     }
   }
 
   case class Mod(x: Char, y: String) extends Instruction {
-    override def exec(state: State): State = {
-      state.inc().setRegister(x, state.getValue(x) % state.getValue(y))
+    override def exec(): Long = {
+      registry(x) = registry(x) % resolve(y)
+      1
     }
+  }
+
+  case class Rcv(x: Char) extends Instruction {
+    override def isRcv: Boolean = true
+
+    override def exec(): Long = if (registry(x) > 0) 0 else 1
   }
 
   def parse(cmd: String): Instruction = {
     val parts = cmd.split(' ')
-    val args: (Char, String) = (parts(1).head, parts.last)
+    val args: (String, String) = (parts(1), parts.last)
     parts.head match {
       case "jgz" => Jmp(args._1, args._2)
-      case "set" => Set(args._1, args._2)
-      case "snd" => Snd(args._1)
-      case "add" => Add(args._1, args._2)
-      case "mul" => Mul(args._1, args._2)
-      case "mod" => Mod(args._1, args._2)
-      case "rcv" => Rcv(args._1)
+      case "set" => Set(args._1.head, args._2)
+      case "snd" => Snd(args._1.head)
+      case "add" => Add(args._1.head, args._2)
+      case "mul" => Mul(args._1.head, args._2)
+      case "mod" => Mod(args._1.head, args._2)
+      case "rcv" => Rcv(args._1.head)
     }
   }
 
-  private def runProgram(instructions: Seq[Instruction], state: State): State = {
-    state match {
-      case s@State(pos, _, _) =>
-        instructions(pos.toInt) match {
-          case Snd(r) => Snd(r).exec(s)
-          case Rcv(x) => Rcv(x).exec(s)
-          case Set(x, y) => Set(x, y).exec(s)
-          case Add(x, y) => Add(x, y).exec(s)
-          case Mul(x, y) => Mul(x, y).exec(s)
-          case Mod(x, y) => Mod(x, y).exec(s)
-          case Jmp(x, y) => Jmp(x, y).exec(s)
-        }
+  def exec(instructions: List[Instruction]): Long = {
+
+    @tailrec
+    def accumulator(pos: Int): Long = pos match {
+      case p if p < instructions.length =>
+        val offset: Long = instructions(pos.toInt).exec()
+        if (offset == 0 && instructions(pos.toInt).isRcv) registry('_') else accumulator(p + offset.toInt)
+      case _ => throw new NoSuchElementException
     }
+
+    accumulator(0)
   }
 
   override def run(): Any = {
-    val instructions: List[Instruction] = Source.fromResource("day18.txt").getLines().map(parse).toList
-
-    def program(program: State): State = runProgram(instructions, program)
-
-    def isBlocked(p: State) = p.isBlocked || p.position < 0 || p.position >= instructions.length
-
-    val run: Iterator[State] = Iterator.iterate(State.empty())(x => program(x.copy()))
-
-    run.find(isBlocked).get.getValue('_')
+    val input = Source.fromResource("day18.txt").getLines.toList
+    exec(input.map(parse))
   }
-
 
 }
