bbec9f52e8e6e4ef209e50c4373034616944479e
Matti Lupari <matti.lupari@arcusys.fi>
2017-12-18 22:14:21 +0200

    Day 18

--- src/main/scala/challenge/Day18b.scala
+++ src/main/scala/challenge/Day18b.scala
@@ -2,136 +2,128 @@ package challenge
 
 import base.Challenge
 
-import scala.collection.immutable
+import scala.collection.mutable
 import scala.io.Source
 
 object Day18b extends Challenge {
 
-  case class State(position: Long, registry: Map[Char, Long], source: immutable.Queue[Long], sink: immutable.Queue[Long], sendCount: Long, isBlocked: Boolean) {
-    def inc() = copy(position = position + 1)
-
-    def setRegister(r: Char, value: Long) = copy(registry = registry + (r -> value))
-
-    def getValue(arg: String): Long = if (arg.last.isDigit) arg.toInt else registry.getOrElse(arg.last, 0)
-
-    def getValue(arg: Char): Long = registry.getOrElse(arg, 0)
-  }
-
-  object State {
-    def empty(id: Long): State = State(0, Map('p' -> id), immutable.Queue.empty, immutable.Queue.empty, 0, isBlocked = true)
-  }
-
-  sealed trait Instruction {
-    def exec(state: State): State
+  sealed abstract class Instruction {
+    def exec(registry: mutable.Map[Char, Long]): Long
 
     def isRcv: Boolean = false
-
     def isSnd: Boolean = false
 
+    def resolve(arg: String, registry: mutable.Map[Char, Long]): Long = if (arg.last.isDigit) arg.toInt else registry(arg.last)
   }
 
   case class Jump(x: String, y: String) extends Instruction {
-
-    override def exec(state: State): State = {
-      val inc = if (state.getValue(x) > 0) state.getValue(y) else 1
-      state.copy(position = state.position + inc)
+    override def exec(registry: mutable.Map[Char, Long]): Long = {
+      if (resolve(x, registry) > 0) resolve(y, registry) else 1
     }
   }
 
-  case class Snd(x: Char) extends Instruction {
-
-    override def exec(state: State): State = {
-      state.inc().copy(sink = state.sink.enqueue(state.getValue(x)), sendCount = state.sendCount + 1)
-    }
-  }
-
-  case class Rcv(x: Char) extends Instruction {
-
-    override def exec(state: State): State = {
-      if (state.source.isEmpty) state.copy(isBlocked = true)
-      else {
-        val (m: Long, q: immutable.Queue[Long]) = state.source.dequeue
-        state.inc().setRegister(x, m).copy(source = q)
-      }
+  case class Snd(x: Char, queue: mutable.Queue[Long], id: Int) extends Instruction {
+    override def isSnd: Boolean = true
+    override def exec(registry: mutable.Map[Char, Long]): Long = {
+      queue.enqueue(registry(x))
+      1
     }
   }
 
   case class Set(x: Char, y: String) extends Instruction {
-
-    override def exec(state: State): State = {
-      state.inc().setRegister(x, state.getValue(y))
+    override def exec(registry: mutable.Map[Char, Long]): Long = {
+      registry(x) = resolve(y, registry)
+      1
     }
   }
 
   case class Add(x: Char, y: String) extends Instruction {
-
-    override def exec(state: State): State = {
-      state.inc().setRegister(x, state.getValue(x) + state.getValue(y))
+    override def exec(registry: mutable.Map[Char, Long]): Long = {
+      registry(x) += resolve(y, registry)
+      1
     }
   }
 
   case class Mul(x: Char, y: String) extends Instruction {
-
-    override def exec(state: State): State = {
-      state.inc().setRegister(x, state.getValue(x) * state.getValue(y))
+    override def exec(registry: mutable.Map[Char, Long]): Long = {
+      registry(x) *= resolve(y, registry)
+      1
     }
   }
 
   case class Mod(x: Char, y: String) extends Instruction {
-
-    override def exec(state: State): State = {
-      state.inc().setRegister(x, state.getValue(x) % state.getValue(y))
+    override def exec(registry: mutable.Map[Char, Long]): Long = {
+      registry(x) = registry(x) % resolve(y, registry)
+      1
     }
   }
 
-  def parse(cmd: String): Instruction = {
-    val parts = cmd.split(' ')
-    val args: (String, String) = (parts(1), parts.last)
-    parts.head match {
-      case "jgz" => Jump(args._1, args._2)
-      case "set" => Set(args._1.head, args._2)
-      case "snd" => Snd(args._1.head)
-      case "add" => Add(args._1.head, args._2)
-      case "mul" => Mul(args._1.head, args._2)
-      case "mod" => Mod(args._1.head, args._2)
-      case "rcv" => Rcv(args._1.head)
+  case class Rcv(x: Char, queue: mutable.Queue[Long], id: Int) extends Instruction {
+
+    override def isRcv: Boolean = true
+
+    override def exec(registry: mutable.Map[Char, Long]): Long = {
+      if (queue.nonEmpty) {
+        registry(x) = queue.dequeue()
+        1
+      } else {
+        0
+      }
     }
+  }
+
+  case class Program(id: Int, source: mutable.Queue[Long], sink: mutable.Queue[Long], input: List[String], var pointer: Long = 0) {
+    val registry: mutable.Map[Char, Long] = mutable.Map[Char, Long]('p' -> id).withDefaultValue(0)
+    var sendCount: Int = 0
+    val instructions: List[Instruction] = input.map(parse)
+
+    def parse(cmd: String): Instruction = {
+      val parts = cmd.split(' ')
+      val args: (String, String) = (parts(1), parts.last)
+      parts.head match {
+        case "jgz" => Jump(args._1, args._2)
+        case "set" => Set(args._1.head, args._2)
+        case "snd" => Snd(args._1.head, sink, id)
+        case "add" => Add(args._1.head, args._2)
+        case "mul" => Mul(args._1.head, args._2)
+        case "mod" => Mod(args._1.head, args._2)
+        case "rcv" => Rcv(args._1.head, source, id)
+      }
+    }
+
+
+    def isRunning: Boolean = !instructions(pointer.toInt).isRcv || !source.isEmpty
+
+    def isHalted: Boolean = pointer < 0 || pointer >= instructions.length
+
+    def run(): Unit = {
+      val instruction = instructions(pointer.toInt)
+      val offset: Long = instruction.exec(registry)
+      if (instruction.isSnd) sendCount = sendCount + 1
+      pointer += offset
+    }
+
   }
 
   override def run(): Any = {
-    val instructions: List[Instruction] = Source.fromResource("day18.txt").getLines().map(parse).toList
+    val input = Source.fromResource("day18.txt").getLines.toList
+    val queue1: mutable.Queue[Long] = mutable.Queue.empty
+    val queue2: mutable.Queue[Long] = mutable.Queue.empty
+    val prog1: Program = Program(0, source=queue1, sink=queue2, input)
+    val prog2: Program = Program(1, source=queue2, sink=queue1, input)
 
-    def program(program: State): Iterator[State] = runProgram(instructions, program)
-
-    def isBlocked(p: State) = p.isBlocked || p.position < 0 || p.position >= instructions.length
-
-    val run = Iterator.iterate((State.empty(0), State.empty(1))) {
-      case (p0, p1) =>
-        (
-          program(p0.copy(isBlocked = false, sink = immutable.Queue.empty, source = p1.sink)).find(isBlocked).get,
-          program(p1.copy(isBlocked = false, sink = immutable.Queue.empty, source = p0.sink)).find(isBlocked).get
-        )
+    while (prog1.isRunning || prog2.isRunning) {
+      while (!prog1.isHalted && prog1.isRunning) {
+        prog1.run()
+      }
+      while (!prog2.isHalted && prog2.isRunning) {
+        prog2.run()
+      }
     }
-    run.drop(1).find {
-      case (p0, p1) =>
-        p0.sink.isEmpty &&
-          p1.sink.isEmpty
-    }.get._2.sendCount
+    prog2.sendCount
 
   }
 
-  private def runProgram(instructions: Seq[Instruction], state: State): Iterator[State] = {
-    Iterator.iterate(state) {
-      case s@State(position, _, _, _, _, _) =>
-        instructions(position.toInt) match {
-          case Snd(r) => Snd(r).exec(s)
-          case Set(x, y) => Set(x, y).exec(s)
-          case Add(x, y) => Add(x, y).exec(s)
-          case Mul(x, y) => Mul(x, y).exec(s)
-          case Mod(x, y) => Mod(x, y).exec(s)
-          case Jump(x, y) => Jump(x, y).exec(s)
-          case Rcv(x) => Rcv(x).exec(s)
-        }
-    }
-  }
 }
+
+
