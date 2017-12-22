80644406f88327f6261c196237d0f74908ae10bd
Matti Lupari <matti.lupari@arcusys.fi>
2017-12-22 09:30:18 +0200

    cleanup

--- src/main/scala/challenge/Day21.scala
+++ src/main/scala/challenge/Day21.scala
@@ -7,45 +7,51 @@ import scala.io.Source
 
 object Day21 extends Challenge {
 
-  type Grid = Vector[String]
+  case class Grid(v: Vector[String]) {
+
+    def flips(grid: Grid): GridList = Vector(Grid(grid.v.reverse), Grid(grid.v.map(_.reverse)))
+
+    def rotations(): GridList = {
+      def rotate90(l: Grid): Grid = Grid(l.v.transpose.map(_.reverse).map(_.mkString))
+
+      (1 to 3).foldLeft(Vector(rotate90(this)))((a, _) => a :+ rotate90(a.last))
+    }
+
+    def variations(): Set[Grid] = {
+      val rotated: GridList = this +: rotations()
+      val flipped: GridList = rotated.flatMap(flips)
+      (rotated ++ flipped).toSet
+    }
+
+    def divide(): GridList = {
+      val n: Int = if (v.size % 2 == 0) 2 else 3
+      v.grouped(n).map(g => g.map(s => s.grouped(n).toList).transpose).toVector.flatten.map(Grid)
+    }
+
+    def count(): Int = v.map(_.count(_ == '#')).sum
+
+  }
+
   type GridList = Vector[Grid]
-
-  // memoization grid -> rule
-  val memo: mutable.Map[Grid, Grid] = mutable.Map()
-
-  def flip(grid: Grid): GridList = Vector(grid.reverse, grid.map(_.reverse))
-
-  def rotate(grid: Grid): GridList = {
-    def rotate90(l: Grid): Grid = l.transpose.map(_.reverse).map(_.mkString)
-
-    (1 to 3).foldLeft(Vector(rotate90(grid)))((a, _) => a :+ rotate90(a.last))
-  }
-
-  def matches(grid: Grid): Set[Grid] = {
-    val rotated: GridList = rotate(grid) :+ grid
-    val flipped: GridList = rotated.flatMap(flip)
-    (rotated ++ flipped).toSet
-  }
-
-  def divide(grid: Grid): GridList = {
-    val n: Int = if (grid.size % 2 == 0) 2 else 3
-    grid.grouped(n).map(g => g.map(s => s.grouped(n).toList).transpose).toVector.flatten
-  }
 
   def join(grids: GridList): Grid = {
     val gs: Int = math.sqrt(grids.length.toDouble).toInt
     gs match {
       case 1 => grids.head
       case _ =>
-        val ts: Int = grids.head.length
-        grids.grouped(gs).map(g => (0 until ts).map(i => g.indices.map(j => g(j)(i)).mkString)).flatten.toVector
+        val ts: Int = grids.head.v.length
+        val v = grids.grouped(gs).map(g => (0 until ts).map(i => g.indices.map(j => g(j).v(i)).mkString)).flatten.toVector
+        Grid(v)
     }
   }
+
+  // memoization grid -> rule
+  val memo: mutable.Map[Grid, Grid] = mutable.Map()
 
   def transform(grid: Grid, rules: Map[Grid, Grid]): Grid = {
     if (memo.contains(grid)) memo(grid)
     else {
-      val rule: Grid = rules.keys.find(k => matches(grid).contains(k)).get
+      val rule: Grid = rules.keys.find(k => grid.variations().contains(k)).get
       val transformation = rules(rule)
       memo(grid) = transformation
       transformation
@@ -55,18 +61,17 @@ object Day21 extends Challenge {
   def parse(line: String): (Grid, Grid) = {
     val parts = line.split(" => ")
     val rule = List(parts.head, parts.last).map(_.split("/").toVector.map(_.mkString))
-    (rule.head, rule.last)
+    (Grid(rule.head), Grid(rule.last))
   }
 
   def execute(limit: Int): Int = {
-    val input: Grid = Source.fromResource("day21.txt").getLines().toVector
+    val input: List[String] = Source.fromResource("day21.txt").getLines.toList
     val transformations: Map[Grid, Grid] = input.map(parse).toMap
-    val result: Grid = (1 to limit).zipWithIndex.foldLeft(Vector(".#.", "..#", "###"))((a, b) => {
-      val replacements: GridList = divide(a).map(transform(_, transformations))
-      // no need to join during the last round
-      if (b._2 == limit -1) replacements.flatten else join(replacements)
+    val result: Grid = (1 to limit).zipWithIndex.foldLeft(Grid(Vector(".#.", "..#", "###")))((a, b) => {
+      val replacements: GridList = a.divide().map(transform(_, transformations))
+      join(replacements)
     })
-    result.map(_.count(_ == '#')).sum
+    result.count()
   }
 
   override def run(): Any = execute(5)
