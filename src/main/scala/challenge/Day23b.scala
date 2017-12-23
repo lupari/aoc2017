f8a301643ea154893034b91b99b8e40de1861513
Matti Lupari <matti.lupari@arcusys.fi>
2017-12-23 22:35:45 +0200

    add explanation

--- src/main/scala/challenge/Day23b.scala
+++ src/main/scala/challenge/Day23b.scala
@@ -43,32 +43,29 @@ object Day23b extends Challenge {
   |
   |b = c = 65
   |if (a != 0)
-  |	b = b * 100 + 100000
-  |	c = b + 17000
+  |    b = b * 100 + 100000
+  |    c = b + 17000
   |do
-  |	f = 1
-  |	d = 2
-  |	do
-  |		e = 2
-  |		do
-  |			g = d * e -b
-  |			if (g == 0)
-  |				f = 0
-  |			e = e +1
-  |			g = e - b
-  |			sub g b
-  |		while g != 0
-  |
-  |		d = d+1
-  |		g = d - b
-  |	while g != 0
-  |	if (f != 0)
-  |		h = h + 1
-  |
-  |	g = b - c
-  |	if (g != 0)
-  |		break
-  |	b = b + 17
+  |    f = 1
+  |    d = 2
+  |    do
+  |        e = 2
+  |        do
+  |            g = d * e -b
+  |            if (g == 0)
+  |                f = 0
+  |            e = e + 1
+  |            g = e - b
+  |        while g != 0
+  |        d = d+1
+  |        g = d - b
+  |    while g != 0
+  |    if (f != 0)
+  |        h = h + 1
+  |    g = b - c
+  |    if (g != 0)
+  |        break
+  |    b = b + 17
   |while (true)
   |
   |===============
@@ -76,14 +73,14 @@ object Day23b extends Challenge {
   |b = 106500
   |c = 123500
   |for (b in range(106500, c , 17) {
-  | 	f = 1
-  |	  for d in range(2, b, 1) {
-  |		  for (e in range(2, b, 1)
-  |			  if (d * e == b) // Find two numbers (d, e) whose product is b i.e. making b a non-prime number
-  |         f = 0
-  |		  }
-  |	  }
-  |	  if (f == 0) h = h + 1 // increment counter if non-prime was found
+  |    f = 1
+  |    for d in range(2, b, 1) {
+  |        for (e in range(2, b, 1)
+  |            if (d * e == b) // Find two numbers (d, e) whose product is b i.e. making b a non-prime number
+  |                f = 0
+  |        }
+  |    }
+  |    if (f == 0) h = h + 1 // increment counter if non-prime was found
   |}
   |
 */
