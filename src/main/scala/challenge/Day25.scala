211560b3fce266187806299e9b90d5eca56a8b7d
Matti Lupari <matti.lupari@arcusys.fi>
2017-12-28 09:58:41 +0200

    cleanup

--- src/main/scala/challenge/Day25.scala
+++ src/main/scala/challenge/Day25.scala
@@ -2,33 +2,38 @@ package challenge
 
 import base.Challenge
 
+import scala.annotation.tailrec
+
 object Day25 extends Challenge {
 
   def play(limit: Int): Int = {
 
-    def accumulator(pos: Int, state: Char, i: Int, tape: Map[Int, Boolean]): Int = i match {
+    @tailrec
+    def accumulator(i: Int, pos: Int, state: Char, tape: Map[Int, Boolean]): Int = i match {
       case x if x == limit => tape.values.count(_ == true)
-      case _ => state match {
-        case 'a' =>
-          val v = tape(pos)
-          accumulator(pos + (if (!v) 1 else -1), 'b', i + 1, tape + (pos -> !v))
-        case 'b' =>
-          val v = tape(pos)
-          accumulator(pos + (if (!v) 1 else -1), if (!v) 'c' else 'b', i + 1, tape)
-        case 'c' =>
-          val v = tape(pos)
-          accumulator(pos + (if (!v) 1 else -1), if (!v) 'd' else 'a', i + 1, tape + (pos -> !v))
-        case 'd' =>
-          val v = tape(pos)
-          accumulator(pos - 1, if (!v) 'e' else 'f', i + 1, tape + (pos -> true))
-        case 'e' =>
-          val v = tape(pos)
-          accumulator(pos -1, if (!v) 'a' else 'd', i + 1, tape + (pos -> !v))
-        case 'f' =>
-          val v = tape(pos)
-          accumulator(pos + (if (!v) 1 else -1), if (!v) 'a' else 'e', i + 1, tape + (pos -> true))
-        case _ => throw new NoSuchElementException
-      }
+      case _ =>
+        val v = tape(pos)
+        state match {
+          case 'a' =>
+            if (v) accumulator(i + 1, pos - 1, 'b', tape + (pos -> false))
+            else accumulator(i + 1, pos + 1, 'b', tape + (pos -> true))
+          case 'b' =>
+            if (v) accumulator(i + 1, pos - 1, 'b', tape)
+            else accumulator(i + 1, pos + 1, 'c', tape)
+          case 'c' =>
+            if (v) accumulator(i + 1, pos - 1, 'a', tape + (pos -> false))
+            else accumulator(i + 1, pos + 1, 'd', tape + (pos -> true))
+          case 'd' =>
+            if (v) accumulator(i + 1, pos - 1, 'f', tape + (pos -> true))
+            else accumulator(i + 1, pos - 1, 'e', tape + (pos -> true))
+          case 'e' =>
+            if (v) accumulator(i + 1, pos - 1, 'd', tape + (pos -> false))
+            else accumulator(i + 1, pos - 1, 'a', tape + (pos -> true))
+          case 'f' =>
+            if (v) accumulator(i + 1, pos - 1, 'e', tape + (pos -> true))
+            else accumulator(i + 1, pos + 1, 'a', tape + (pos -> true))
+          case _ => throw new NoSuchElementException
+        }
 
     }
 
